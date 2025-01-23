198d234b498de4f01ee0eeaccb619bc7b0ee6d81
Karl <joissaintkarl@gmail.com>
2025-01-23 21:12:38 +0100

    minor corrections

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -106,14 +106,14 @@ export default function Users() {
     <>
       <Navbar />
 
-      <div className="pt-20 p-3 max-w-6xl mx-auto">
-        <div className="mb-4">
+      <div className=" pt-20 p-3 max-w-6xl mx-auto">
+        <div className="mb-4 flex justify-center">
           <input
             type="text"
             placeholder="Search users by name or email..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full md:w-1/2 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full md:w-1/2 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
           />
         </div>
 
